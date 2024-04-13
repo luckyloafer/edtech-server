12dd71c904a4d5fef6220fcad1cb9e85c4efa074
patnana lakshmana murthy <patnanalakshmanamurthy9@gmail.com>
2024-04-13 12:05:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const cors = require('cors');
+const stripe = require('stripe')(process.env.STRIPEKEY)
+app.use(cors());
+
+
+app.post('/create-checkout-session',async(req,res)=>{
+    const {products} = req.body;
+
+    const lineItems = products.map((product)=>({
+        price_data:{
+            currency:'inr',
+            product_data:{
+                name:product.productName,
+                images:[product.productImg]
+            },
+            unit_amount:product.price*100, 
+        },
+        quantity:1
+    }))
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:['card'],
+        line_items:lineItems,
+        mode:'payment',
+        success_url:'http://localhost:5173/paymentSuccess',
+        cancel_url:'http://localhost:5173/paymentFailure'
+    })
+
+    res.json({id:session.id})
+})
+
+
+app.listen(3000,()=>{
+    console.log('server running')
+})
